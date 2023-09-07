db7f1acd5212a1f61c60123639f9a3d803b0b7fa
Pingvu <nhlpool27@gmail.com>
2023-09-07 13:14:56 +0200

    Add project files.

---
+++ Uppgift2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Your computer has crashed.");
+            Console.ReadLine();
+        }
+    }
+}
